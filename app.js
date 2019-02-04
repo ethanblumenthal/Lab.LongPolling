9619ceed1dc2dc9d7471219d34a8f7feff6865d6
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2019-02-04 09:30:50 -0600

    completed lab

--- app.js
+++ app.js
@@ -6,11 +6,33 @@ app.get('/', (req, res) => {
     <html>
       <head>
         <script type="text/javascript">
-          console.log('hello world!')
+        console.log('hello world!')
+        function longPollForTime () {
+          fetch('/the-time', { headers: { 'Cache-Control': 'no-cache' } })
+            .then(response => response.text())
+            .then(time => {
+              console.log('The time is:', time)
+              longPollForTime()
+            })
+          }
+          longPollForTime()
         </script>
       </head>
     </html>
   `)
+})
+
+const { EventEmitter } = require('events')
+const clock = new EventEmitter()
+setInterval(() => {
+  const time = (new Date()).toLocaleString()
+  clock.emit('tick', time)
+}, 5000)
+
+clock.on('tick', time => console.log('The time is ', time))
+
+app.get('/the-time', (req, res) => {
+  clock.once('tick', time => res.send(time))
 })
 
 app.listen(3333, () => {
